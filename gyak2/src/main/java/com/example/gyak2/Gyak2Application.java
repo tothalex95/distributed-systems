7ba07f43cad0705cdbe4723f784b7ca7810e7d08
Alex Toth <tothalex95@gmail.com>
2019-09-25 16:52:15 +0200

    gyak2 springboot project

---
+++ gyak2/src/main/java/com/example/gyak2/Gyak2Application.java
@@ -0,0 +1,13 @@
+package com.example.gyak2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gyak2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gyak2Application.class, args);
+    }
+
+}
