a392fda1acc292ea5800c9d990fd43b72561bcd6
Alex Toth <tothalex95@gmail.com>
2019-10-02 14:15:30 +0200

    gyak3 spring boot project

---
+++ gyak3/src/main/java/com/example/gyak3/Gyak3Application.java
@@ -0,0 +1,13 @@
+package com.example.gyak3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gyak3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gyak3Application.class, args);
+    }
+
+}
