10f6f36f56e33be0cf9d7636879c81470ccf517e
Alex Toth <tothalex95@gmail.com>
2019-11-06 14:29:33 +0100

    test fixed

--- fbchatbot/src/test/java/com/example/fbchatbot/service/FacebookServiceTest.java
+++ fbchatbot/src/test/java/com/example/fbchatbot/service/FacebookServiceTest.java
@@ -1,10 +1,12 @@
 package com.example.fbchatbot.service;
 
 import com.example.fbchatbot.model.Event;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
@@ -21,10 +23,15 @@ class FacebookServiceTest {
     @InjectMocks
     private FacebookService facebookService;
 
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
     @Test
     void sendResponse() {
         Mockito.when(facebookApiEndpoints.getFacebookSendUrl()).thenReturn("FB_URL");
-        Mockito.when(restTemplate.postForEntity("FB_URL", Mockito.any(Event.class), String.class))
+        Mockito.when(restTemplate.postForEntity(Mockito.eq("FB_URL"), Mockito.any(Event.class), Mockito.eq(String.class)))
         .thenReturn(ResponseEntity.ok("asd"));
 
         assertEquals(ResponseEntity.ok("asd"), facebookService.sendResponse(new Event()));
